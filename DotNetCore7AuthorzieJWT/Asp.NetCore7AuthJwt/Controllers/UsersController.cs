f1bce26416b78d145a6422195640adb75211113c
shaghi <mojtaba.shagi@gmail.com>
2023-08-29 18:46:52 +0330

    Produces Attribute Web Api

---
+++ DotNetCore7AuthorzieJWT/Asp.NetCore7AuthJwt/Controllers/UsersController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Asp.NetCore7AuthJwt.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    //[Authorize]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/<UsersController>
+        [Produces("application/json")]
+        [HttpGet]
+        public async Task<IActionResult> Get() => Ok(new { data = new List<string> { "value1", "value2" }});
+
+
+        [HttpGet("list2")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<string>) )]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //Error in IActionResult =Task<IActionResult<IEnumerable<string>>>
+        public async Task<ActionResult<IEnumerable<string>>> GetList2() => Ok(await Task.FromResult( new List<string> { "user1", "user2" }));
+
+
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("list3")]
+        public async Task<IEnumerable<string>> GetList3() => await Task.FromResult(new List<string> { "user1", "user2" });
+
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet("list4")]
+        public async Task<IEnumerable<string>> GetList4() => await Task.FromResult(new List<string> { "user1", "user2" });
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public string Get(int id)
+        {
+            
+            return "value";
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- DotNetCore7AuthorzieJWT/Asp.NetCore7AuthJwt/Program.cs
+++ DotNetCore7AuthorzieJWT/Asp.NetCore7AuthJwt/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+addAuthhenticationJWTService(builder.Services, builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,3 +25,24 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+ void addAuthhenticationJWTService(IServiceCollection services,IConfiguration config)
+{
+    const string JwtKeyOptionsNameInAppSettings= "JwtKeyOptions";
+    services.AddAuthentication(Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+
+            options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateActor = true,
+                ValidAudience = config[$"{JwtKeyOptionsNameInAppSettings}:ValidAudience"],
+                ValidIssuer = config[$"{JwtKeyOptionsNameInAppSettings}:ValidIssuer"],
+                ClockSkew = TimeSpan.Zero,
+                IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(config[$"{JwtKeyOptionsNameInAppSettings}:Secret"]))
+            };
+        });
+}
